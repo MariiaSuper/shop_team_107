2abb02586c7828fbfbf1c01270d3e0f3474e8e6c
Vasyl Chukhrai <wasik1995@mail.com>
2024-09-23 20:48:35 +0300

    add some miui header test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,33 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { Box, Button } from '@mui/material';
+
 export const Header = () => {
-  return <div>header</div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          My Application
+        </Typography>
+
+        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Contact</Button>
+        </Box>
+
+        <IconButton edge="end" color="inherit">
+          <AccountCircle />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
 };
