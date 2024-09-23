56c17e7a24d5baff2a4d01280954e3d0ecaec3bd
Vasyl Chukhrai <wasik1995@mail.com>
2024-09-23 15:18:15 +0300

    install-redux-toolkit

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,13 +1,17 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import App from './App';
 import { HomePage } from './components/HomePage';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 export const Root = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<HomePage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
