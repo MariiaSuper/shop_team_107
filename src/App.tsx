f8e60fe55c194366dee2042007c8d1ec3f22c9dc
Mari Andrushko <115041780+MariiaSuper@users.noreply.github.com>
2024-09-24 18:45:01 +0200

    st-30: download fonts and icons from MUI and add a few icons for testing (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import TestComponent from './components/TestComponent';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { PaletteColor } from '@mui/material/styles/createPalette';
+import IconExample from './components/IconExample';
 
 // 1. Define your custom palette
 declare module '@mui/material/styles' {
@@ -67,6 +68,7 @@ function App() {
         <Footer />
 
         <TestComponent />
+        <IconExample />
       </div>
     </ThemeProvider>
   );
