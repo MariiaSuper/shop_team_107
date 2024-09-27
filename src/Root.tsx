c5ffb203655effa0c01f9ebff115056d9b618a1c
Mari Andrushko <115041780+MariiaSuper@users.noreply.github.com>
2024-09-27 16:07:05 +0200

    st-34: create carousel component (#9)

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,8 +1,9 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import App from './App';
-import { HomePage } from './components/HomePage';
+import { HomePage } from './components/HomePage/HomePage';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { NotFoundPage } from './components/NotFoundPage';
 
 export const Root = () => (
   <Provider store={store}>
@@ -10,6 +11,7 @@ export const Root = () => (
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/CarouselComponent/Carousel.tsx
@@ -0,0 +1,71 @@
+import Carousel from 'react-material-ui-carousel';
+import { Card, CardMedia, Box, useTheme } from '@mui/material';
+
+const items = [
+  {
+    image: '/images/Banner1@2x.png'
+  },
+  {
+    image: '/images/Banner2@2x.png'
+  },
+  {
+    image: '/images/Banner3@2x.png'
+  },
+  {
+    image: '/images/Banner4@2x.png'
+  }
+];
+
+function MobileCarousel() {
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ width: '100%' }} mt={2}>
+      <Carousel
+        autoPlay={true}
+        animation="slide"
+        indicators={true}
+        duration={500}
+        navButtonsAlwaysVisible={false}
+        navButtonsAlwaysInvisible={false}
+        cycleNavigation={true}
+        fullHeightHover={true}
+        swipe={true}
+        indicatorIconButtonProps={{
+          style: {
+            width: '11px',
+            height: '11px',
+            marginRight: '12px',
+            color: theme.palette.primary.light,
+            border: `1px solid ${theme.palette.primary.main}`
+          }
+        }}
+        activeIndicatorIconButtonProps={{
+          style: {
+            color: theme.palette.primary.main
+          }
+        }}
+        indicatorContainerProps={{
+          style: {
+            marginTop: '8px',
+            textAlign: 'center'
+          }
+        }}>
+        {items.map((item, index) => (
+          <Card key={index}>
+            <CardMedia
+              component="img"
+              image={item.image}
+              sx={{
+                // minHeight: 200,
+                objectFit: 'cover'
+              }}
+            />
+          </Card>
+        ))}
+      </Carousel>
+    </Box>
+  );
+}
+
+export default MobileCarousel;
