da8b5c1e96a2cd97e953b1c1263d458e136acbd5
Mari Andrushko <115041780+MariiaSuper@users.noreply.github.com>
2024-10-02 19:38:33 +0200

    st-36: create about us component (#13)
    * st-36: create about us component
    
    * st-36: create about us component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import { CarouselComponent } from '../../components/CarouselComponent';
 import { Box } from '@mui/material';
 import Catalog from '../../components/Catalog/Catalog';
+import { AboutUs } from '../../components/AboutUs';
 
 export const HomePage = () => {
   return (
     <Box display="flex" flexDirection="column" mx={2}>
       <CarouselComponent />
       <Catalog />
+      <AboutUs />
     </Box>
   );
 };
